8f47385b0bb5e4419325ba2d4b763ad361fc9145
Li YuJing <yujing.li@rea-group.com>
2015-09-01 20:45:41 +0800

    add two classes

---
+++ src/main/java/CartItem.java
@@ -0,0 +1,20 @@
+/**
+ * Created by twer on 9/1/15.
+ */
+public class CartItem {
+    private Goods goods;
+    private int quantity;
+    private  double costs;
+
+    public Goods getGoods() {
+        return goods;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getCosts() {
+        return costs;
+    }
+}
---
+++ src/main/java/Goods.java
@@ -0,0 +1,25 @@
+/**
+ * Created by twer on 9/1/15.
+ */
+public class Goods {
+    private String name;
+    private double  price;
+    private double tax;
+    private double cost;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
