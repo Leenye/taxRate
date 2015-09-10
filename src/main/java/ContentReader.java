097def9f055727820d969321bf73ab9dc63063fd
Li YuJing <yujing.li@rea-group.com>
2015-09-11 00:13:10 +0800

    extract tax calculator

--- src/main/java/ContentReader.java
+++
@@ -1,9 +0,0 @@
-import java.io.BufferedReader;
-import java.util.List;
-
-/**
- * Created by twer on 9/6/15.
- */
-public interface ContentReader {
-    public List<Cart> readContent(BufferedReader reader);
-}
